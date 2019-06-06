2016e2569b4587ee69d25b8666be164d0dd35aea
unknown <pwhynotfilms@gmail.com>
2019-06-05 19:52:08 -0500

    orm, config and seeds edited

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,80 @@
+const express = require("express");
 const connection = require("../config/connection");
 
+function printQuestionMarks(num) {
+   const arr = [];
+  
+    for (let i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+  function objToSql(ob) {
+    const arr = [];
+
+    for (const key in ob) {
+      const value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+    return arr.toString();
+  }
+
+  const orm = {
+    selectAll : function (cb){
+        connection.query("SELECT * FROM tacos;", function(err, data) {
+            if (err) {
+            }
+            cb(data);
+        });
+    },
+    insertOne: function( cols, vals, cb) {
+        const queryString = "INSERT INTO tacos";
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+    },
+
+    updateOne: function (){
+        connection.query(function(err) {
+            if (err) throw err;
+            connection.query(`UPDATE ${item} FROM tacos`, function (err, res){
+                console.log(res);
+            })
+        });
+    },
+    delete: function(table, condition, cb) {
+        const queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
+  };
+
+  module.exports = orm;
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require("express");
-
-const PORT = process.env.PORT || 8080;
-
-const app = express();
-
-app.use(express.static("public"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+const app = express();
+const path = require("path");
+const orm = require("./config/orm");
+
+const PORT = process.env.PORT || 3000;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(express.static(path.join (__dirname + "/public")));
+app.use(express.static(path.join (__dirname + "/models")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const routes = require("./controllers/tacos_controllers");
 
